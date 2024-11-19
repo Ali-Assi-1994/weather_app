1ff92ce6a0cc626081c11b500059b04f07065faa
Ali Assi <alisouliemanassi@gmail.com>
2024-11-19 13:10:39 +0300

    correct usage of url and query parameters

--- lib/ApiClient/urls.dart
+++ lib/ApiClient/urls.dart
@@ -1,3 +1,3 @@
 abstract class URLS {
-  static const String forecast = '/forecast?lat=44.34&lon=10.99&appid=e5fb42624d70e76deff42c44b69388d3';
+  static const String forecast = '/forecast';
 }
--- lib/features/weather/data/weather_repository.dart
+++ lib/features/weather/data/weather_repository.dart
@@ -8,7 +8,11 @@ class WeatherRepository {
   WeatherRepository({required this.apiClient});
 
   Future<WeatherModel> fetchData() async {
-    ApiResult result = await apiClient.get(url: URLS.forecast, queryParameters: {});
+    ApiResult result = await apiClient.get(url: URLS.forecast, queryParameters: {
+      "lat": "44.34",
+      "lon": "10.99",
+      "appid": const String.fromEnvironment('appid', defaultValue: 'e5fb42624d70e76deff42c44b69388d3'),
+    });
     if (result.type == ApiResultType.success) {
       await Future.delayed(const Duration(seconds: 1));
       return WeatherModel.fromJson(result.data);
